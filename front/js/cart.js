2122727cc3f945bb429a8e17b0280a67ae1a6d0f
Marie Guigou <guigoumarie@gmail.com>
2023-01-13 12:10:31 +0100

    fixes #04, adding fonctions to get total price, total quantity, to delete items and to change quantity. Some are to be reviewed.

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,20 +1,33 @@
-// Cart recovery from the local storage
-const cart = JSON.parse(localStorage.getItem("Canape"));
-console.log(cart);
+const productInLocalStorage = getCart();
+console.log(productInLocalStorage);
 
-let totalPrice = 0;
+//Get datas
+
+function fetchProductsInCart() {
+  for (const element of productInLocalStorage) {
+    // Look at the product on the local Storage
+    fetch("http://localhost:3000/api/products/" + element._id) // Get the informations of this products from the API
+      .then((response) => response.json())
+      .then((productInCart) => {
+        displayProduct(productInCart, element.color, element.quantity);
+        console.log(productInCart)
+      })
+
+      .catch((err) => {
+        document
+          .getElementsByTagName("h1")[0]
+          .insertAdjacentHTML("beforebegin", `Une erreur est survenue(${err})`);
+      });
+  }
+}
 
 //empty and full cart management
-if (cart === null || cart.length === 0) {
+if (productInLocalStorage === null || productInLocalStorage.length === 0) {
   document.getElementsByTagName("h1")[0].innerHTML = "Votre panier est vide";
 } else {
   fetchProductsInCart();
-  //total quantity
-  const totalQuantities = cart.reduce(
-    (total, item) => total + item.quantity,
-    0
-  );
-  document.getElementById("totalQuantity").innerHTML = totalQuantities;
+  getTotalQuantity();
+  getTotalPrice();
 }
 
 function displayProduct(product, colors, quantity) {
@@ -42,16 +55,78 @@ function displayProduct(product, colors, quantity) {
               </article>
           `;
 }
-
-function fetchProductsInCart() {
-  for (const element of cart) {
-    // Look at the product on the local Storage
-    fetch("http://localhost:3000/api/products/" + element._id) // Get the informations of this products from the API
-      .then((response) => response.json())
-      .then((productInCart) => {
-        displayProduct(productInCart, element.color, element.quantity);
-        totalPrice += productInCart.price * element.quantity;
-        document.getElementById("totalPrice").innerHTML = totalPrice;
-      });
+// Cart recovery from the local storage
+function getCart() {
+  let cart = localStorage.getItem("Canape");
+  if (cart) {
+    return JSON.parse(cart);
+  } else {
+    return [];
   }
+}
+
+function getTotalPrice() {
+  productInLocalStorage.forEach((e) => {
+    fetch("http://localhost:3000/api/products/" + e._id)
+      .then((response) => response.json())
+      .then((datas) => {
+        console.log(datas);
+        let totalPrice = 0;
+        for (let item in datas) {
+          console.log("ici" + e.quantity);
+          totalPrice += datas.price * e.quantity;
+          document.getElementById("totalPrice").innerHTML = totalPrice;
+        }
+      });
+  });
+}
+
+//total quantity
+function getTotalQuantity() {
+  const totalQuantities = productInLocalStorage.reduce(
+    (total, item) => total + item.quantity,
+    0
+  );
+  document.getElementById("totalQuantity").innerHTML = totalQuantities;
+}
+
+
+let clickedItem = document.querySelector(".cart__item");
+let clickedItemId = clickedItem.dataset
+console.log("mon id" + clickedItemId)
+
+// const searchItem = productInLocalStorage.find (
+//   (element) => element._id == clickedItem.dataset.id
+// );
+
+
+
+
+//delete items
+// function deleteItem() {
+//   document.querySelector(".deleteItem").addEventListener("click", () => {
+//     const itemToDelete = productInLocalStorage.find(
+//       (element) => element._id == dataset.id
+//     );
+//     return itemToDelete;
+//   });
+// }
+
+function changeQuantity() {
+  document
+    .querySelector("#itemQuantity")
+    .addEventListener("change", function () {
+      // Recovery of form datas onclick
+      const duplicatedItems = productInLocalStorage.find(
+        (element) => element._id == productInLocalStorage._id
+      );
+      if (duplicatedItems != undefined) {
+        duplicatedItems.quantity += productInLocalStorage.quantity;
+        if (duplicatedItems.quantity <= 0) {
+          deleteItem(duplicatedItems); // fonction de suppression
+        }
+      }
+      productInLocalStorage.push(duplicatedItems);
+      localStorage.setItem("Canape", JSON.stringify(productInLocalStorage));
+    });
 }
