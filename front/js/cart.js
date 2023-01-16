d4c5bc8026d404113ee7c88415ed4037b9a0196b
Marie Guigou <guigoumarie@gmail.com>
2023-01-16 10:03:16 +0100

    fixes #04, function to delete items ok

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,33 +1,27 @@
-const productInLocalStorage = getCart();
-console.log(productInLocalStorage);
-sort(); // Grouping of sofas by id in the cart display
-
 //Get datas from the API
 function fetchProductsInCart() {
   // Look at the product on the local Storage
-  for (const element of productInLocalStorage) {
-    fetch("http://localhost:3000/api/products/" + element._id) // Get the informations of this products from the API
+  getSortedCart().forEach((productInCart) => {
+    fetch("http://localhost:3000/api/products/" + productInCart._id) // Get the informations of this products from the API
       .then((response) => response.json())
-      .then((productInCart) => {
-        displayProduct(productInCart, element.color, element.quantity); // display of cart items
+      .then((product) => {
+        displayProduct(product, productInCart.color, productInCart.quantity); // display of cart items
+        deleteItem();
+        changeQuantity();
       })
 
       .catch((err) => {
         document
-          .getElementsByTagName("h1")[0]
+          .querySelector("h1")
           .insertAdjacentHTML("beforebegin", `Une erreur est survenue(${err})`);
       });
-  }
+  });
 }
 
-//empty and full cart management
-if (productInLocalStorage === null || productInLocalStorage.length === 0) {
-  document.getElementsByTagName("h1")[0].innerHTML = "Votre panier est vide";
-} else {
-  fetchProductsInCart(); // display of cart items
-  getTotalQuantity(); // Insert total quantity of product
-  getTotalPrice(); // Insert total price
-}
+setEmptyCart(); //empty cart management
+fetchProductsInCart(); // display of cart items
+getAndRenderTotalQuantity(); // Insert total quantity of product
+getAndRenderTotalPrice(); // Insert total price
 
 // display of cart items
 function displayProduct(product, colors, quantity) {
@@ -65,74 +59,99 @@ function getCart() {
   }
 }
 
+//Grouping sofas by id in the cart display
+function getSortedCart() {
+  const sortedCart = getCart().sort((a, b) => (a._id < b._id ? -1 : 1));
+  return sortedCart;
+}
+
 //total price
-function getTotalPrice() {
+function getAndRenderTotalPrice() {
   let totalPrice = 0;
-  productInLocalStorage.forEach((e) => {
-    fetch("http://localhost:3000/api/products/" + e._id)
+  getCart().forEach((product) => {
+    fetch("http://localhost:3000/api/products/" + product._id)
       .then((response) => response.json())
       .then((datas) => {
-        console.log(datas);
-        totalPrice += datas.price * e.quantity;
+        totalPrice += datas.price * product.quantity;
         document.getElementById("totalPrice").innerHTML = totalPrice;
       });
   });
 }
 
 //total quantity
-function getTotalQuantity() {
-  const totalQuantities = productInLocalStorage.reduce(
+function getAndRenderTotalQuantity() {
+  const totalQuantities = getCart().reduce(
     (total, item) => total + item.quantity,
     0
   );
   document.getElementById("totalQuantity").innerHTML = totalQuantities;
 }
 
-deleteItem();
 //delete items
 function deleteItem() {
-  let deleteButton = document
-    .querySelectorAll(".deleteItem")
-    .closest(".cart__item"); // select all the delete buttons
-  let clickedItem = deleteButton.closest(".cart__item"); // find the node parent of the delete button clicked
-  // let clickedItem = document.querySelector(".cart__item").parentElement.closest(deleteButton);
-  deleteButton.forEach((button) => {
-    addEventListener("click", () => {
-      let clickedItemId = clickedItem.dataset;
-      let itemToDelete = productInLocalStorage.find(
-        (element) => element._id == dataset.id && element.color == dataset.color
+  let newCart = getCart();
+  const deleteButtons = document.querySelectorAll(".deleteItem"); // select all the delete button
+  deleteButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      // find the product to delete
+      let removeItem = button.closest(".cart__item");
+      let removeItemId = removeItem.dataset.id; 
+      let removeItemColor = removeItem.dataset.color; 
+      // select the product to delete
+      let itemToDelete = newCart.find(
+        (element) =>
+          element._id == removeItemId && element.color == removeItemColor
       );
-      console.log(itemToDelete);
+      if (itemToDelete) {
+        //keep all the items of the cart exepted the clicked one
+        newCart = newCart.filter((i) => i !== itemToDelete);
+        console.log(newCart);
+        // save the new cart in local storage
+        localStorage.setItem("Canape", JSON.stringify(newCart));
+        removeItem.remove(); // remove the item to delete
+        setEmptyCart(); // message if the cart is empty
+        getAndRenderTotalQuantity(); // uptade and render new quantity
+        getAndRenderTotalPrice(); //uptade and render new total price
+      }
     });
   });
 }
-
+//message if the cart is empty
+function setEmptyCart() {
+  if (getCart() === null || getCart().length === 0) {
+    document.getElementsByTagName("h1")[0].innerHTML = "Votre panier est vide";
+  }
+}
 // Change quantity
-let quantityToChange = parseInt(
-  document.getElementsByClassName("itemQuantity").value
-);
-let itemQuantity = document.querySelector(".itemQuantity");
-itemQuantity.addEventListener("input", () =>
-  changeQuantity(productInLocalStorage._id, quantityToChange)
-);
+function changeQuantity() {
+  //Get all ItemQuantity element and set for each of them
+  const itemQuantities = document.querySelectorAll(".itemQuantity");  
 
-function changeQuantity(_id, quantityToChange, productInLocalStorage) {
-  newQuantity = productInLocalStorage.find(
-    (element) => element._id == productInLocalStorage._id
-  );
-  if (duplicatedItems != undefined) {
-    newQuantity.quantity = quantityToChange;
-  }
-  if (duplicatedItems.quantity <= 0) {
-    deleteItem(duplicatedItems); // fonction de suppression
-  }
-  localStorage.setItem("Canape", JSON.stringify(productInLocalStorage));
-  getTotalQuantity();
-  getTotalPrice();
-}
+  itemQuantities.forEach((itemQuantity) => {
+    //listen to each of itemquanity
+    itemQuantity.addEventListener("change", (event) => {
+      event.preventDefault();
+      //find node that match :class cart__item
+      
+      let quantityToChange = itemQuantity.closest(".cart__item");
+      console.log(quantityToChange);
 
-//Grouping of sofas by id in the cart display
-function sort() {
-  productInLocalStorage.sort((a, b) => (a._id < b._id ? -1 : 1));
+      //Doublecheck if products found in cart and set id & color of modified product to the same one
+      const productFound = getCart().find(
+        (item) =>
+          item._id == quantityToChange.dataset.id &&
+          item.color == quantityToChange.dataset.color
+      );
+
+      // if it's found in cart, return new number of quantity to value
+      if (productFound) {
+        productFound.quantity = parseInt(itemQuantity.value);
+        //Add new info to local storage
+        localStorage.setItem("Canape", JSON.stringify(getCart()));
+        //caculate new totalQuantity and new totalPrice
+        getAndRenderTotalQuantity();
+        getAndRenderTotalPrice();
+      }
+    });
+  });
 }
-console.log(productInLocalStorage);
